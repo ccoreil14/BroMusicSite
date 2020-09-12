35ddcba1099b79ac5c2eb7f420de0a2cd86c0bb9
Christian <ccoreil14@gmail.com>
2020-09-12 14:48:16 -0500

    Page 2 ability added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
  
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html")
