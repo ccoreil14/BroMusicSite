54104154c84e63e998e1787ccb8e103ff5225a22
Christian <ccoreil14@gmail.com>
2020-09-12 14:16:42 -0500

    Style baby

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
  
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html")
 })
